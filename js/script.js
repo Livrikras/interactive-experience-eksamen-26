cb4fd4c6ec151642e2165f87d91ba017d586f5c9
Livrikras <livrikras@icloud.com>
2026-05-19 13:48:37 +0200

    startet på array i js

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// Array på værkerne
+
+const art = [
+    {
+        id: 1,
+        title: "En erindring efter Danmarks største martyr",
+        artist: "Frits Albert Florintinus Strand",
+        material: "Olie på lærred",
+        size: "56x49 cm",
+    },
+    {
+        id: 2,
+        title: "ingen titel",
+        artist: "Jette Hansen",
+        material: "Olie på lærred",
+    },
+    {
+        id: 3,
+        title: "ingen titel",
+        artist: "Marie Heiberg",
+        material: "Olie på lærred",
+    },
+    {
+        id: 4,
+        year: 1951-2012,
+        title: "Portræt",
+        artist: "Ole Norge",
+        material: "Oliekridt",
+        size: "31x44 cm",
+        about portrait: "En ung mand med gylden hårpragt og sortrandende øjne ser lige igennem os - Øjnene er skræmte",
+        about artist: "Ole Norges værker er farverige, men udtrykker også en skrøbelighed. Værkerne opløses i farvefelter eller mekaniske linjer og former.",
+        psychiatry: "Ole Norge var i kontakt med psykiatrien, efter at han i en ung alder havde en psykotisk episode, hvor han identificerede sig med Jesus. Han oplevede det som forløsende at male og tegne.",
+    },
+    {
+        id: 5,
+        year: 1951-1986,
+        title: "Luder og Madonna",
+        artist: "Patrick Larsen",
+        material: "Acryl og oliekridt",
+        size: "54x50 cm",
+        about portrait: "Patrick Larsen portrætter samfundets bagside, og skildrer magtesløshed og nederlag. Hans maleteknik med farver i flere lag er mættet, og det giver billederne følelsesmæssig intensitet. Gul er en gennemgående farve -mellem smerte og ulykke.",
+        about artist: "Patrick Larsen havde et hårdt liv og en alt for tidlig dø. I hans værker bevæger han sig tit omkring temaerne død, angst og lidelse",
+        psychiatry: "Hårde miljøer var rammen om hans eget liv og alt for tidlige død. De mørke følelser har ligget i Patrick Larsens eget sind og har koblet med en stor følsomhed har det medført hårde sammenstød med tilværelsen.",
+    }
+]
