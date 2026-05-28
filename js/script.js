b6209a63890510154387b6e824f59f6dab360fe8
Anne-sofie112 <eaa25asabr@students.eaaa.dk>
2026-05-28 10:02:18 +0200

    når video er færdig går den tilbage til baggrund

--- js/script.js
+++ js/script.js
@@ -21,3 +21,9 @@ const savedVideo = localStorage.getItem("selectedPortrait");
 if (savedVideo) {
   showVideo(savedVideo);
 }
+
+
+videoElement.addEventListener("ended", () => {
+  videoElement.src = "";
+  document.body.style.backgroundColor = "#564A45";
+});
