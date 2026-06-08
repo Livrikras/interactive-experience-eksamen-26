76788f08b53c6c71f3bea7fa04d95a79c6a3140d
Livrikras <livrikras@icloud.com>
2026-06-08 11:26:26 +0200

    Update screen.js

--- js/screen.js
+++ js/screen.js
@@ -169,7 +169,6 @@ function openProjectionWindow() {
 
   return projectionWindow;
 }
-
 // Vis knappen til at stoppe videoafspilningen
 function showStopProjectionButton() {
   const stopButtons = document.querySelectorAll(".stop-projection-btn");
@@ -181,7 +180,6 @@ function hideStopProjectionButton() {
   const stopButtons = document.querySelectorAll(".stop-projection-btn");
   stopButtons.forEach((button) => button.classList.add("is-hidden"));
 }
-
 // Koden tjekker om projectionWindow findes og er åbent.
 // Hvis ja: sendes en besked { action: "stop" } til det andet browser-vindue via postMessage().
 gallery.addEventListener("click", (e) => {
@@ -204,7 +202,6 @@ artScreen.forEach((item) => {
     <div class="image-side">
       <img src="${item.image}" alt="${item.name}" class="painting">
     </div>
-
     <div class="info-side">
       <button class="stop-projection-btn is-hidden" type="button">X</button>
       <span class="card-year">${item.year}</span>
